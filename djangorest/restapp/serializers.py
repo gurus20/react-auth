b5a176d366ce4b104ba23997304b23434c53f956
Gurdayal Singh <guru.gps.20@gmail.com>
2023-05-09 17:22:26 +0530

    clear

--- djangorest/restapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from restapp.models import Todo
-
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = "__all__"
